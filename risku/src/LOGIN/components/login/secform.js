c9ed4789508806fb156a9b3ba7b14011247fccf1
adityatandon14 <tandon.aditya@yahoo.com>
2020-10-11 14:46:54 +0530

    comment

--- risku/src/LOGIN/components/login/secform.js
+++ risku/src/LOGIN/components/login/secform.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 import "./styles.css";
+/* isme i need to make 2 apis 
+
+localhost8000/api/userdata/id for existing button
+
+localhost8000/api/userdata for new patient button
+
+then when i submit calculator.js i to post that forms data on respective api
+*/
 const secform = () => (
   <form className="frm">
     <div className="top">
