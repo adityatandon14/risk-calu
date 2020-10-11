b34214c39427d99d59ace3d428a9456f35c4a9f0
adityatandon14 <tandon.aditya@yahoo.com>
2020-10-11 14:39:57 +0530

    secform

--- risku/src/LOGIN/components/login/secform.js
+++ risku/src/LOGIN/components/login/secform.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
-
+import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import "./styles.css";
 const secform = () => (
-  <Form>
-    <Form.Field>
+  <form className="frm">
+    <div className="top">
       <label>ENTER PATIENT ID</label>
       <input placeholder="ID" />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label="I agree to the Terms and Conditions" />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
+    </div>
+    <div className="bon">
+      <Button type="submit">Existing Patient</Button>
+      <Button type="submit">New Patient</Button>
+    </div>
+  </form>
 );
 
 export default secform;
